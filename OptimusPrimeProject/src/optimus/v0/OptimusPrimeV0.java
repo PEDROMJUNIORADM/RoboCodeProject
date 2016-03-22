0b141522786e27e464bcf4bd0a4e1d860bb58dd5
Lucas Albuquerque de Almeida <lucas.ufcg@gmail.com>
2016-03-22 00:02:47 -0300

    Optimus Prime with Technical shoot.

---
+++ OptimusPrimeProject/src/optimus/v0/OptimusPrimeV0.java
@@ -0,0 +1,47 @@
+package optimus.v0;
+
+import robocode.AdvancedRobot;
+import robocode.HitByBulletEvent;
+import robocode.HitWallEvent;
+import robocode.ScannedRobotEvent;
+
+/**
+ * A simple OptimusPrime roboot, 
+ * this is a default version of a roboot.
+ * 
+ * @version 0.0
+ * @author Lucas
+ *
+ */
+public class OptimusPrimeV0 extends AdvancedRobot {
+
+	public void run() {
+		while(true) {
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		back(20);
+	}	
+}
